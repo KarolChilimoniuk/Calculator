64ef3b3fdbcb231e97f45e0e4580959984d81bff
KarolChilimoniuk <webdevkarolchilimoniuk@gmail.com>
2020-04-22 20:03:20 +0200

    feat: added result paragraph and its css styles

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,21 @@
 
 // service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("serviceworker.js").then(
+      function (registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        // registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
   });
 }
 
@@ -19,64 +25,67 @@ if ('serviceWorker' in navigator) {
 // ---- create class for calculator
 class Calculator {
   constructor(operators, numbers) {
-      this.operators = operators,
-      this.numbers = numbers,
-      this.result = `0`
+    (this.operators = operators), (this.numbers = numbers), (this.result = `0`);
   }
   getResult(operators, numbers, result) {
-     this.numbers.forEach(number => {
-       number.addEventListener('click', () => {
-        if(this.result === `0`) {
+    this.numbers.forEach((number) => {
+      number.addEventListener("click", () => {
+        if (this.result === `0`) {
           this.result = number.textContent;
         } else {
           this.result = this.result + number.textContent;
         }
-          resultScreen.textContent = this.result;
-       });
-     });
-     this.operators.forEach(operator => {
-       operator.addEventListener('click', () => {
-        switch(operator.textContent) {
-        case "ce":
-          this.result = `0`;
-          break;
-        case "+":
-          this.result = this.result + "+";
-          break;
-        case "-":
-          this.result = this.result + "-";
-          break;
-        case "del":
-          if(this.result !== `0`) {
-            this.result = this.result.substring(0, this.result.length - 1)
-          } 
-          if(this.result === ``) {
+        resultScreen.textContent = this.result;
+      });
+    });
+    this.operators.forEach((operator) => {
+      operator.addEventListener("click", () => {
+        switch (operator.textContent) {
+          case "ce":
             this.result = `0`;
-          }
-          break;
-        case "/":
-          this.result = this.result + "/";
-          break;
-        case "*":
-          this.result = this.result + "*";
-          break;
-        case ".":
-          this.result = this.result + ".";
-          break;
-      case "=":
-          this.result = `${eval(this.result)}`;
-          break;
+            break;
+          case "+":
+            this.result = this.result + "+";
+            break;
+          case "-":
+            this.result = this.result + "-";
+            break;
+          case "del":
+            if (this.result !== `0`) {
+              this.result = this.result.substring(0, this.result.length - 1);
+            }
+            if (this.result === ``) {
+              this.result = `0`;
+            }
+            break;
+          case "/":
+            this.result = this.result + "/";
+            break;
+          case "*":
+            this.result = this.result + "*";
+            break;
+          case ".":
+            this.result = this.result + ".";
+            break;
+          case "=":
+            this.result = `${eval(this.result)}`;
+            break;
         }
         resultScreen.textContent = this.result;
-     })
-    })
+      });
+    });
   }
 }
 
 // ---- get result
 
-const resultScreen = document.querySelector('.calculator__result--js');
-const calculator = new Calculator([...document.querySelectorAll('.button__action--js')], [...document.querySelectorAll('.button__number--js')]);
+const resultScreen = document.querySelector(
+  ".calculator__paragraph-result--js"
+);
+const calculator = new Calculator(
+  [...document.querySelectorAll(".button__action--js")],
+  [...document.querySelectorAll(".button__number--js")]
+);
 resultScreen.textContent = calculator.result;
 
 calculator.getResult();
@@ -116,5 +125,5 @@ console.log(
 ░░░░░░░░░░█▄▄▄▄▐▄▄▄▄█
 ░░░░░░░▄▄▄▄▀▄▄▀█▀▄▄▀▄▄▄▄
 ░░░░░▄▀▄░▄░▄░░░█░░░▄░▄░▄▀▄
-░░░░░█▄▄▄▄▄▄▄▄▄▀▄▄▄▄▄▄▄▄▄`)
-
+░░░░░█▄▄▄▄▄▄▄▄▄▀▄▄▄▄▄▄▄▄▄`
+);
